0a5620648fa873a05b4cb9a29751738250795d53
Joseph Jojo <jjojo@CORP.FFIC.COM>
2019-04-03 20:15:41 +0530

    Navigation - App bars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
 import { MatSidenavModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    // Material
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
