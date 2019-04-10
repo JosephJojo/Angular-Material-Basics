80ded2cb92887a596ff614aa3863c049a85c8400
Joseph Jojo <jjojo@CORP.FFIC.COM>
2019-04-10 19:22:02 +0530

    Form Controls - Inputs

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerCardComponent } from './customer-card/customer-card.component';
+import { CustomerNewComponent } from './customer-new/customer-new.component';
 
 // Material Components
 import { MatButtonModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
 
 @NgModule({
-  declarations: [CustomerListComponent, CustomerCardComponent],
+  declarations: [CustomerListComponent, CustomerCardComponent, CustomerNewComponent],
   imports: [
     CommonModule,
     CustomersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
 
     // Material Components
     MatButtonModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule
   ]
 })
 export class CustomersModule { }
