e36e52d47b1faafdd02db3fa1d63dd646c61aa29
Joseph Jojo <jjojo@CORP.FFIC.COM>
2019-04-11 00:24:03 +0530

    Form Controls - Date picker

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
+import { OrderNewComponent } from './order-new/order-new.component';
 
 // Material Components
+import { MatButtonModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
 import { MatTabsModule } from '@angular/material';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 @NgModule({
-  declarations: [OrderListComponent],
+  declarations: [OrderListComponent, OrderNewComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     OrdersRoutingModule,
-    MatTabsModule
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    MatTabsModule,
+    MatMomentDateModule
   ]
 })
 export class OrdersModule { }
